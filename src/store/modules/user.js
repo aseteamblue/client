b5aec86809b307577b2d7534ced4254d023dd05b
marom17 <romain.flac@gmail.com>
2018-11-02 12:24:27 +0100

    display session status in user profile TG-20

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,13 +1,49 @@
+import axios from 'axios'
+import config from '../../../config'
+import auth from './auth'
+
 const state = {
-  username: 'test'
+  sessions: []
 }
 
 const mutations = {
-
+  addSession (state, data) {
+    state.sessions.push(data)
+  },
+  searchSession (sate) {
+    state.sessions = []
+  }
 }
 
 const actions = {
-
+  getUserSession ({ commit, rootState }) {
+    return new Promise((resolve, reject) => {
+      commit('searchSession')
+      axios({ url: config.api_url + '/users/' + auth.state.user._id + '/sessions/', method: 'GET' })
+        .then(resp => {
+          for (let i = 0; i < resp.data.length; i++) {
+            actions.getSessionInfo({ commit }, resp.data[i])
+          }
+          resolve(resp)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    })
+  },
+  getSessionInfo ({ commit }, sessionId) {
+    return new Promise((resolve, reject) => {
+      axios({ url: config.api_url + '/sessions/' + sessionId, method: 'GET' })
+        .then(resp => {
+          let info = resp.data
+          commit('addSession', info)
+          resolve(resp)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    })
+  }
 }
 
 export default {
