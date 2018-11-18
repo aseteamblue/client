62d3bcda73e5083c505c44567b6e9e781ca41d1d
Johan Jobin <26439579+JobinJohan@users.noreply.github.com>
2018-11-18 12:05:56 +0100

    First dynamic visualization

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -25,7 +25,7 @@
                 <option value="co2">CO2</option>
               </select>
             </td>
-            <td v-if="!s.active"> <button v-on:click="generateVisualizations(s._id, selected[index])">Generate graphs</button></td>
+            <td v-if="!s.active"> <button v-on:click="generateVisualizations(s._id, selected[index], index)">Generate graphs</button></td>
           </tr>
         </tbody>
       </table>
@@ -42,13 +42,14 @@
 
 <script>
 
-// import * as d3 from 'd3'
-// import Vue from 'vue'
+import * as d3 from 'd3'
+
 // const data = [99, 71, 78, 25, 36, 92]
 export default {
 
   data: function () {
     return {
+      index: '',
       selected: [],
       sessions: this.$store.state.user.sessions,
       dataOfSession: this.$store.state.session.sessionData
@@ -61,7 +62,8 @@ export default {
 
   },
   methods: {
-    generateVisualizations: function (id, selected) {
+    generateVisualizations: function (id, selected, index) {
+      this.index = index
       this.$store.dispatch('sessionDetails', id).then(() => {
         this.$store.dispatch('getSessionData', selected).then(() => {
           this.dataOfSession = this.$store.state.session.sessionData
@@ -72,25 +74,28 @@ export default {
     },
 
     createGraph: function () {
-      return 0
-      // console.log(this.dataOfSession)
-      // const svg = d3.select(this.$el)
-      //   .append('svg')
-      //   .attr('width', 500)
-      //   .attr('height', 270)
-      //   .append('g')
-      //   .attr('transform', 'translate(0, 10)')
-      // const x = d3.scaleLinear().range([0, 430])
-      // const y = d3.scaleLinear().range([210, 0])
-      // d3.axisLeft().scale(x)
-      // d3.axisTop().scale(y)
-      // x.domain(d3.extent(data, (d, i) => i))
-      // y.domain([0, d3.max(data, d => d)])
-      // const createPath = d3.line()
-      //   .x((d, i) => x(i))
-      //   .y(d => y(d))
-      //   console.log(this.dataOfSession)
-      // svg.append('path').attr('d', createPath(data))
+      var test = []
+      for (var d in this.dataOfSession) {
+        // console.log(this.dataOfSession[d].data)
+        test.push(this.dataOfSession[d].data)
+      }
+
+      const svg = d3.select(this.$el)
+        .append('svg')
+        .attr('width', 500)
+        .attr('height', 270)
+        .append('g')
+        .attr('transform', 'translate(0, 10)')
+      const x = d3.scaleLinear().range([0, 430])
+      const y = d3.scaleLinear().range([210, 0])
+      d3.axisLeft().scale(x)
+      d3.axisTop().scale(y)
+      x.domain(d3.extent(test, (d, i) => i))
+      y.domain([0, d3.max(test, d => d)])
+      const createPath = d3.line()
+        .x((d, i) => x(i))
+        .y(d => y(d))
+      svg.append('path').attr('d', createPath(test))
     }
   },
 }
