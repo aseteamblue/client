3908958cd091fee13c0ffe51f1158178af281af6
Thomas Schaller <thomasschaller647@gmail.com>
2018-12-14 16:30:27 +0100

    average stats okay

--- src/components/session/components/AverageStatistics.vue
+++ src/components/session/components/AverageStatistics.vue
@@ -5,15 +5,15 @@
                   <div class='col-md-3'>
                     <div class="layers bd bgc-white p-20">
                       <div class="layer w-100 mB-10">
-                        <h6 class="lh-1">Total Visits</h6>
+                        <h6 class="lh-1">Average Speed</h6>
                       </div>
                       <div class="layer w-100">
                         <div class="peers ai-sb fxw-nw">
                           <div class="peer peer-greed">
-                            <i class="fas fa-user"></i>
+                            <i class="fas fa-2x fa-tachometer-alt c-green-500"></i>
                           </div>
                           <div class="peer">
-                            <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-green-50 c-green-500">+10%</span>
+                            <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-green-50 c-green-500">{{session.averageSpeed}} km/h</span>
                           </div>
                         </div>
                       </div>
@@ -24,15 +24,15 @@
                   <div class='col-md-3'>
                     <div class="layers bd bgc-white p-20">
                       <div class="layer w-100 mB-10">
-                        <h6 class="lh-1">Total Page Views</h6>
+                        <h6 class="lh-1">Average Temperature</h6>
                       </div>
                       <div class="layer w-100">
                         <div class="peers ai-sb fxw-nw">
                           <div class="peer peer-greed">
-                            <span id="sparklinedash2"></span>
+                            <i class="fas fa-2x fa-thermometer-three-quarters c-red-500"></i>
                           </div>
                           <div class="peer">
-                            <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-red-50 c-red-500">-7%</span>
+                            <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-red-50 c-red-500">{{session.averageTemperature}} degrees</span>
                           </div>
                         </div>
                       </div>
@@ -43,15 +43,15 @@
                   <div class='col-md-3'>
                     <div class="layers bd bgc-white p-20">
                       <div class="layer w-100 mB-10">
-                        <h6 class="lh-1">Unique Visitor</h6>
+                        <h6 class="lh-1">Total Distance</h6>
                       </div>
                       <div class="layer w-100">
                         <div class="peers ai-sb fxw-nw">
                           <div class="peer peer-greed">
-                            <span id="sparklinedash3"></span>
+                            <i class="fas fa-2x fa-map-marked-alt c-purple-500"></i>
                           </div>
                           <div class="peer">
-                            <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-purple-50 c-purple-500">~12%</span>
+                            <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-purple-50 c-purple-500">{{session.totalDistance}} km</span>
                           </div>
                         </div>
                       </div>
@@ -62,15 +62,15 @@
                   <div class='col-md-3'>
                     <div class="layers bd bgc-white p-20">
                       <div class="layer w-100 mB-10">
-                        <h6 class="lh-1">Bounce Rate</h6>
+                        <h6 class="lh-1">Duration</h6>
                       </div>
                       <div class="layer w-100">
                         <div class="peers ai-sb fxw-nw">
                           <div class="peer peer-greed">
-                            <span id="sparklinedash4"></span>
+                            <i class="fas fa-2x fa-stopwatch c-blue-500"></i>
                           </div>
                           <div class="peer">
-                            <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-blue-50 c-blue-500">33%</span>
+                            <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-blue-50 c-blue-500">{{msToTime(session.duration)}}</span>
                           </div>
                         </div>
                       </div>
@@ -79,3 +79,33 @@
                 </div>
               </div>
 </template>
+
+<script>
+export default {
+  name: 'averageStats',
+  props: {
+    session: {
+      type: Object,
+      required: true
+    // default: 'Lucas'
+    }
+  },
+  data: function () {
+    return {
+    }
+  },
+
+  methods: {
+    msToTime: function (s) {
+      const ms = s % 1000
+      s = (s - ms) / 1000
+      const secs = s % 60
+      s = (s - secs) / 60
+      const mins = s % 60
+      const hrs = (s - mins) / 60
+
+      return hrs + 'h ' + mins + 'min ' + secs + 'sec'
+    },
+  }
+}
+</script>
